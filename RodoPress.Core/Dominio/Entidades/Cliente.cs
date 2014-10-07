a2437e14e2e749be424f9cc706acfb991d1a712a
daniel <dansouza-95@uninove.edu.br>
2014-10-07 12:49:51 -0300

    Início do projeto

---
+++ RodoPress.Core/Dominio/Entidades/Cliente.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RodoPress.Core.Dominio.Entidades
+{
+    public class Cliente
+    {
+        public String Cpf { get; set; }
+        public String Nome { get; set; }
+        public String Rg { get; set; }
+        public String Endereco { get; set; }
+        public String Email { get; set; }
+        public String DataNascimento { get; set; }
+    }
+}
---
+++ RodoPress.Core/Dominio/Entidades/Funcionario.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RodoPress.Core.Dominio.Entidades
+{
+    public class Funcionario
+    {
+        public int Matricula { get; set; }
+    }
+}
